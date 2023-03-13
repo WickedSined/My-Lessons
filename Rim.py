edda9378c3bf5912332b4cc530f62ea9ede4898c
Skororbohatov Sviatoslav <45883075+CarbonSkorobogatov@users.noreply.github.com>
2023-03-13 19:12:42 +0200

    add resolve of rim

--- Rim.py
+++ Rim.py
@@ -1,16 +1,31 @@
 import math
 
-def decoding(N, pow):
-    letters = {
-        '1000': 'M', 
-        '4000': 'MU', 
-        '5000': 'V'
-    }
-    print(N*10**(pow - 1))
 
+def build(number, str_build):
+    if number < 4: 
+        return str_build[0] * number
+    elif number == 4:
+        return str(str_build[0]) + str(str_build[1])
+    elif number == 5: 
+        return str_build[1]
+    elif number < 9: 
+        return str_build[1] + str(str_build[0]) * (number - 5)
+    elif number == 9: 
+        return str(str_build[0]) + str_build[2]
 
-
-number = input("Enter number 1000 - 9999: ")
+def decoding(number, pow):
+    if pow == 1: 
+        build_str = build(number, ["I", "V", "X"])
+    elif pow == 2: 
+        build_str = build(number, ["X", "L", "C"])
+    elif pow == 3:
+        build_str = build(number, ["C", "D", "M"])
+    elif pow == 4: 
+        build_str = build(number, ["M", "U", "Y"])
+        
+    return build_str
+ 
+number = input("Enter number: ")
 
 try: 
     number = int(number)
@@ -22,29 +37,14 @@ if (number < 0):
    print("Data isn't valid")
    exit() 
 
-
-"""
-M - 1000 - 3000
-MU - 4000
-U - V - 5000
-
-
-XXXX
-
-"""
 pow = (math.floor(math.log10(number) + 1))
-decimal = number // 10**(pow - 1)
+str_build = ""
 
-
-decoding(decimal, pow)
-
-if number>=1000 and number<=3000:
-    print("M")
-elif number>3000 and number<5000:
-    print("MU")
-elif number>4999:
-    print("UV")
-
+for i in range(pow, 0, -1): 
     
+    decimal = number // 10**(i - 1)
+    str_build += decoding(decimal, i)
 
- 
+    number -= decimal * 10**(i - 1) 
+    
+print(str_build)
